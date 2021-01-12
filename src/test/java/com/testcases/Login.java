0997f158529b6a8a48e83b0a998cda2c784f951a
anuragdeb3 <anuragdeb8@gmail.com>
2021-01-12 17:54:22 +0530

    Added new test case

---
+++ src/test/java/com/testcases/Login.java
@@ -0,0 +1,49 @@
+package com.testcases;
+
+import java.util.ArrayList;
+import com.anuragdeb.reports.ExtentReporterNG2;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.anuragdeb.helper.BaseFactory;
+import com.anuragdeb.helper.Library;
+import com.pages.LoginPage;
+
+public class Login extends BaseFactory {
+
+	Login() {
+		super();
+	}
+
+	@BeforeSuite
+	public void s() {
+		
+
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		browserFactory();
+
+	}
+
+	@Test
+	public void login() {
+
+		LoginPage loginPg = new LoginPage();
+		loginPg.Login();
+
+	}
+
+	@AfterTest
+	public void afterTest() {
+
+		tearDown();
+	}
+}
